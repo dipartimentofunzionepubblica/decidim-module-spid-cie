b996f21b81ee9d14f6a277cc7efa3dc97d8672b4
Lorenzo Angelone <l.angelone@kapusons.it>
2022-10-26 16:53:33 +0200

    disable email only for spid

--- app/overrides/decidim/devise/omniauth_registrations/new/new_override.rb
+++ app/overrides/decidim/devise/omniauth_registrations/new/new_override.rb
@@ -7,7 +7,7 @@ Deface::Override.new(virtual_path: "decidim/devise/omniauth_registrations/new",
                      name: "disable-email-field",
                      replace: "erb:contains('f.email_field :email')") do
   '
-<%= f.email_field :email, readonly: true %>
+<%= f.email_field :email, readonly: session["decidim-spid.tenant"].present? %>
 <%= f.hidden_field :raw_data %>
 '
 end
